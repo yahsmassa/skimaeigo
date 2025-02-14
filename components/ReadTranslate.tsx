b7c8bf495d0b7d0d672f9a0b7da9502e192306da
yahsmassa <yahsmassa@gmail.com>
2025-02-15 07:09:26 +0900

    bug fix

--- components/ReadTranslate.tsx
+++ components/ReadTranslate.tsx
@@ -11,20 +11,22 @@ export function ReadTranslate({ isSelected, selectedText }: props) {
   return (
     <div className="flex items-center ">
       <button
+        onMouseDown={(e) => e.preventDefault()}
         onClick={() => readSentence(selectedText)}
         disabled={!isSelected}
         className={cn(
-          "ml-2 px-2 py-1  text-white rounded  w-[60px]",
+          "ml-2 px-2 py-1 text-white rounded w-[60px]",
           isSelected ? " bg-blue-500 hover:bg-blue-600" : "bg-gray-500"
         )}
       >
         Read
       </button>
       <button
+        onMouseDown={(e) => e.preventDefault()}
         onClick={() => translateSentence(selectedText)}
         disabled={!isSelected}
         className={cn(
-          "ml-2 px-2 py-1  text-white rounded  w-[60px]",
+          "ml-2 px-2 py-1 text-white rounded w-[60px]",
           isSelected ? " bg-blue-500 hover:bg-blue-600" : "bg-gray-500"
         )}
       >
